c41878c9e7e0cf6777045abdf733f4dccc5a5a3f
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-25 11:22:35 +0200

    Added a profile image to business card UI

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -12,11 +12,22 @@ struct ContentView: View {
         ZStack {
             Color(red:0.09, green: 0.63, blue:0.52, opacity:1.0)
                 .edgesIgnoringSafeArea(.all)
-            Text("Matti Meikäläinen")
-                .font(Font.custom("Pacifico-Regular", size:40))
-                .foregroundColor(.white)
-                .bold()
-                .padding()
+            VStack {
+                Image("eh_profile_pic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160.0, height: 240.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Eetu Hernesniemi")
+                    .font(Font.custom("Pacifico-Regular", size:40))
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+            }
         }
         
     }
